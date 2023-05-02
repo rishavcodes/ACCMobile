827a246a10d5420ea11e8ce18900f792f5600d2c
Victor Zhao <vzhao.tech@gmail.com>
2023-05-02 11:22:37 -0400

    4: Created AccountSettings Page, android rendering confirmed working, iOS rendering currently test-pending

--- Pages/Settings.jsx
+++ Pages/Settings.jsx
@@ -9,16 +9,73 @@ import SearchBar from "../Components/SearchBar";
 import StyledButton from "../Components/StyledButton";
 import { TextInput } from "react-native-paper";
 import landingStyles from "./pageStyles/landingStyles.js";
+import { Switch } from "react-native-paper";
+import settingStyles from "./pageStyles/settingStyles.js";
+
 
 export default function Settings() {
-  const [text, setText] = useState("");
+
+  const [notifications, setNotifications] = useState([
+    { title: "Property has been favourited", toggleStatus: false, id: 0, notificationType: "push" },
+    { title: "Event registration", toggleStatus: false, id: 1, notificationType: "push" },
+    { title: "New blog has been posted", toggleStatus: false, id: 2, notificationType: "push" },
+    { title: "New event has been posted", toggleStatus: false, id: 3, notificationType: "push" },
+    { title: "Visibility", toggleStatus: false, id: 4, notificationType: "privacy" },
+    { title: "Save login info", toggleStatus: false, id: 5, notificationType: "privacy" },
+  ]);
+
+  const toggleSwitch = (id) => {
+    const newNotifications = [...notifications];
+    newNotifications[id].toggleStatus = !newNotifications[id].toggleStatus;
+    setNotifications(newNotifications);
+  }
 
   return (
     <>
       <View style={landingStyles.container}>
-        <Text>Settings Page</Text>
-        <Text>To be Developed</Text>
+        <View style={{ width: "125%", paddingHorizontal: 80, paddingVertical: 175, backgroundColor: "white" }}>
+          <Text style={settingStyles.subHeader}>Account Settings</Text>
+          <Text style={settingStyles.border}>   </Text>
+          <Text style={settingStyles.title}>Push Notifications</Text>
+            
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[0].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[0].toggleStatus} onValueChange={() => toggleSwitch(0)}></Switch>
+          </View>
+          
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[1].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[1].toggleStatus} onValueChange={() => toggleSwitch(1)}></Switch>
+          </View>            
+
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[2].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[2].toggleStatus} onValueChange={() => toggleSwitch(2)}></Switch>
+          </View>  
+
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[3].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[3].toggleStatus} onValueChange={() => toggleSwitch(3)}></Switch>
+          </View>     
+
+          <Text style={settingStyles.title}>Privacy and Security</Text>      
+
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[4].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[4].toggleStatus} onValueChange={() => toggleSwitch(4)}></Switch>
+          </View>   
+
+          <View style={settingStyles.entryView}>
+            <Text style={settingStyles.entryText}>{notifications[5].title}</Text>
+            <Switch color={"#af1a4d"} style={settingStyles.switch} value={notifications[5].toggleStatus} onValueChange={() => toggleSwitch(5)}></Switch>
+          </View>   
+
+        </View>
       </View>
     </>
-  );
-}
+
+
+
+
+
+  )};
